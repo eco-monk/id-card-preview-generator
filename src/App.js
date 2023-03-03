f99ce2210363c5b7de3e66ce848fbb239c14970e
Anand Srinivasan <anand.a.srinivasan@outlook.com>
2023-03-03 19:41:06 +0530

    add route

--- src/App.js
+++ src/App.js
@@ -11,7 +11,9 @@ export default function App() {
   return (
     <Router>
         <Routes>
-          <Route path="/:regNo/:name" element={<CardPreview />} />
+          <Route path="/" element={<CardPreview />}>
+            <Route path='christ/:regNo/:name'></Route>
+          </Route>
         </Routes>
     </Router>
   );
