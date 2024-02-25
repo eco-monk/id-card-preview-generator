d997a41c954172d58486fc9f39767227e108cc14
Anand Srinivasan <anand.a.srinivasan@outlook.com>
2024-02-25 07:59:34 +0530

    basic image

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import bg from './MBA1_01.jpg';
+import student_photo from './student-photo.jpeg';
 import './App.css';
 import {
   useParams,
@@ -18,6 +19,8 @@ export default function App() {
       ),
     },
     {
+      // https://eco-monk.github.io/id-card-preview-generator/#/christ/123/Anand/B.E.%20(CSE)/01-06-2026/11-12-1995/O+/34,%20G.K.Street,%20Kakapalayam%20main%20road,%20Salem%20-%20637501/9095558504
+      
       path: "christ/:regNo/:name/:course/:validity/:dob/:blood/:address/:mobile",
       element: <CardPreview />,
     },
@@ -38,7 +41,7 @@ function CardPreview() {
         <div className='name'>{name}</div>
         <img 
           className='picture' alt="profile-pic"
-          src='https://drive.google.com/uc?export=view&id=1q1A4bIrPf3yWsLaoWntAlZUEnxCAI65y' 
+          src={student_photo}
           width="100px" height="100px" 
         />
         <div className='course'>{course}</div>
