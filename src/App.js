92763908acafa5d3851c10d583327f5138514ce6
Anand Srinivasan <anand.a.srinivasan@outlook.com>
2024-02-25 09:15:03 +0530

    route changes

--- src/App.js
+++ src/App.js
@@ -17,11 +17,11 @@ export default function App() {
       ),
     },
     {
-      /* 
-      https://eco-monk.github.io/id-card-preview-generator/#/home/christ/1/Anand/B.E.%20(CSE)/01-06-2026/11-12-1995/O+/34, G.K.Street, Kakapalayam main road, Salem - 637501/9095558504
-      https://eco-monk.github.io/id-card-preview-generator/#/home/christ/2/Deepa/B.E.%20(CSE)/01-06-2028/23-04-1998/B+/23, Behind Leela palace, Bangalore/7876876887
+      /*
+      https://eco-monk.github.io/id-card-preview-generator/#/christ/1/Anand/B.E.%20(CSE)/01-06-2026/11-12-1995/O+/34, G.K.Street, Kakapalayam main road, Salem - 637501/9095558504
+      https://eco-monk.github.io/id-card-preview-generator/#/christ/2/Deepa/B.E.%20(CSE)/01-06-2028/23-04-1998/B+/23, Behind Leela palace, Bangalore/7876876887
       */
-      path: "home/:institution/:regNo/:name/:course/:validity/:dob/:blood/:address/:mobile",
+      path: "christ/:regNo/:name/:course/:validity/:dob/:blood/:address/:mobile",
       element: <ChristPreview />,
     },
   ]);
--- src/christ/index.js
+++ src/christ/index.js
@@ -2,16 +2,16 @@ import { useParams } from "react-router-dom";
 import "./index.css";
 
 export function ChristPreview() {
-    let { institution, regNo, name, course, validity, dob, blood, address, mobile } = useParams();
+    let { regNo, name, course, validity, dob, blood, address, mobile } = useParams();
     return (
       <div className='wrapper'>
-        <img src={`./media/${institution}/bg.jpg`} className="bg-image" alt="bg" />
+        <img src={`./media/christ/bg.jpg`} className="bg-image" alt="bg" />
         <div className='text-content'>
           <div className='reg-no'>{regNo}</div>
           <div className='name'>{name}</div>
           <img 
             className='picture' alt="profile-pic"
-            src={`./media/${institution}/s-${regNo}.jpg`}
+            src={`./media/christ/s-${regNo}.jpg`}
             width="100px" height="100px" 
           />
           <div className='course'>{course}</div>
