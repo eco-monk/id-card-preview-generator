d7986df5bd4cd44f94ad16edce9d5a37d0458a60
Anand Srinivasan <anand.a.srinivasan@outlook.com>
2024-02-25 08:36:17 +0530

    route changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-import bg from './MBA1_01.jpg';
-import student_photo from './student-photo.jpeg';
 import './App.css';
 import {
   useParams,
@@ -19,9 +17,8 @@ export default function App() {
       ),
     },
     {
-      // https://eco-monk.github.io/id-card-preview-generator/#/christ/123/Anand/B.E.%20(CSE)/01-06-2026/11-12-1995/O+/34,%20G.K.Street,%20Kakapalayam%20main%20road,%20Salem%20-%20637501/9095558504
-      
-      path: "christ/:regNo/:name/:course/:validity/:dob/:blood/:address/:mobile",
+      // https://eco-monk.github.io/id-card-preview-generator/#/home/christ/1/Anand/B.E.%20(CSE)/01-06-2026/11-12-1995/O+/34,%20G.K.Street,%20Kakapalayam%20main%20road,%20Salem%20-%20637501/9095558504
+      path: "home/:institution/:regNo/:name/:course/:validity/:dob/:blood/:address/:mobile",
       element: <CardPreview />,
     },
   ]);
@@ -32,16 +29,16 @@ export default function App() {
 }
 
 function CardPreview() {
-  let { regNo, name, course, validity, dob, blood, address, mobile } = useParams();
+  let { institution, regNo, name, course, validity, dob, blood, address, mobile } = useParams();
   return (
     <div className='wrapper'>
-      <img src={bg} className="bg-image" alt="logo" />
+      <img src={`./media/${institution}/bg.jpg`} className="bg-image" alt="bg" />
       <div className='text-content'>
         <div className='reg-no'>{regNo}</div>
         <div className='name'>{name}</div>
         <img 
           className='picture' alt="profile-pic"
-          src={student_photo}
+          src={`./media/${institution}/s-${regNo}.jpg`}
           width="100px" height="100px" 
         />
         <div className='course'>{course}</div>
