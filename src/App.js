44adc9353260a5a33bd822aad30d23ad8df92156
Anand Srinivasan <anand.a.srinivasan@outlook.com>
2024-02-25 09:00:41 +0530

    component split

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
 import {
-  useParams,
   RouterProvider,
   createHashRouter
 } from "react-router-dom";
+import { CardPreview } from './christ';
 
 export default function App() {
 
@@ -28,29 +28,5 @@ export default function App() {
 
   return (
     <RouterProvider router={router}></RouterProvider>
-  );
-}
-
-function CardPreview() {
-  let { institution, regNo, name, course, validity, dob, blood, address, mobile } = useParams();
-  return (
-    <div className='wrapper'>
-      <img src={`./media/${institution}/bg.jpg`} className="bg-image" alt="bg" />
-      <div className='text-content'>
-        <div className='reg-no'>{regNo}</div>
-        <div className='name'>{name}</div>
-        <img 
-          className='picture' alt="profile-pic"
-          src={`./media/${institution}/s-${regNo}.jpg`}
-          width="100px" height="100px" 
-        />
-        <div className='course'>{course}</div>
-        <div className='validity'>{validity}</div>
-        <div className='dob'>{dob}</div>
-        <div className='blood'>{blood}</div>
-        <div className='address'>{address}</div>
-        <div className='mobile'>{mobile}</div>
-      </div>
-    </div>
   );
 }
